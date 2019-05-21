5e341621b6ada345e537bddb6d20c3d5da70a611
Craig Hanson <craigahanson@gmail.com>
2019-05-21 15:16:46 +0000

    Added Di project and added people controller

---
+++ ProjectX.Di/UnityConfig.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectX.Api;
+using ProjectX.Api.Abstractions;
+
+namespace ProjectX.Di
+{
+    public static class UnityConfig
+    {
+        public static void Build(IServiceCollection services)
+        {
+            services.AddScoped<IPeopleService, PeopleService>();
+        }
+    }
+}
