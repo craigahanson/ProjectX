8e396f303b6c97b9a15bf8a7d97e6cc57d389c08
Craig Hanson <craigahanson@gmail.com>
2019-05-22 21:07:40 +0000

    Added mvc project displaying list of people

---
+++ src/ProjectX.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectX.Mvc.Models;
+
+using ProjectX.Api.Abstractions;
+
+namespace ProjectX.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly IPeopleService peopleService;
+
+        public PeopleController(IPeopleService peopleService)
+        {
+            this.peopleService = peopleService;
+        }
+        public IActionResult Index()
+        {
+            var people = peopleService.GetAll();
+
+            return View(new PeopleModel
+            {
+                People = people.Select(p => new PersonModel
+                {
+                    Id = p.Id,
+                    FirstName = p.FirstName,
+                    LastName = p.LastName
+                })
+            });
+        }
+    }
+}
---
+++ src/ProjectX.Mvc/Models/PersonModel.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+public class PeopleModel
+{
+    public IEnumerable<PersonModel> People { get; set; }
+}
