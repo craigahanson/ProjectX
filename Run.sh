1ab11214a5c4d88cfe352ff3f0e9886b186653e3
Craig Hanson <craigahanson@gmail.com>
2019-10-17 11:15:23 +0000

    Removed angular project

--- Run.sh
+++ Run.sh
@@ -1,4 +1,3 @@
 dotnet run --project src/projectx.webapi &
 dotnet run --project src/projectx.mvc &
-dotnet run --project src/projectx.angular &
 dotnet run --project src/projectx.blazor --urls='http://0.0.0.0:4000;https://0.0.0.0:4001'
