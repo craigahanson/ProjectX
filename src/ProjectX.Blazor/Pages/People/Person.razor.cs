03c64d8a87b2cb1c4f426a1e9f55077389075a60
Craig Hanson <craigahanson@gmail.com>
2019-10-18 20:09:01 +0000

    Seperated code into their own c# classes so we can start doing things like DI etc properly

---
+++ src/ProjectX.Blazor/Pages/People/Person.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ProjectX.Api.Abstractions;
+using System.Net.Http;
+
+namespace ProjectX.Blazor.Pages.People
+{
+    public class PersonModel : ComponentBase {
+        
+        [Parameter] public int Id { get;set; }
+        private HttpClient httpClient;
+        public ApiPerson person;
+
+        public PersonModel()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:1001/api/");
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            person = await httpClient.GetJsonAsync<ApiPerson>($"people/{Id}");
+        }
+    }
+}
---
+++ src/ProjectX.Blazor/Pages/Version/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ProjectX.Api.Abstractions;
+using System.Net.Http;
+
+namespace ProjectX.Blazor.Pages.Version
+{
+    public class IndexModel : ComponentBase {
+        
+        private HttpClient httpClient;
+        public ApiVersion version;
+
+        public IndexModel()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:1001/api/");
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            version = await httpClient.GetJsonAsync<ApiVersion>("version");
+        }
+    }
+}
