69b84051ac87ea2b660d91436695b7fd5e977a28
Craig Hanson <craigahanson@gmail.com>
2019-10-18 11:25:39 +0000

    Changed blazor port

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 dotnet run --project src/projectx.webapi &
 dotnet run --project src/projectx.mvc &
-dotnet run --project src/projectx.blazor --urls='http://0.0.0.0:4000;https://0.0.0.0:4001'
+dotnet run --project src/projectx.blazor --urls='http://0.0.0.0:3000;https://0.0.0.0:3001'
