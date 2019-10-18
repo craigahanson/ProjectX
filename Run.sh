c5ef82333a60008f296046aa48688569acf1d1fd
Craig Hanson <craigahanson@gmail.com>
2019-10-18 10:01:35 +0000

    Added watch to blazor app otherwise we have to keep rebuilding the whole app to see changes

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 dotnet run --project src/projectx.webapi &
 dotnet run --project src/projectx.mvc &
-dotnet run --project src/projectx.blazor --urls='http://0.0.0.0:4000;https://0.0.0.0:4001'
+dotnet watch --project src/projectx.blazor run --urls='http://0.0.0.0:4000;https://0.0.0.0:4001'
