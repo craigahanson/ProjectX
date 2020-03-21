144206b842c55f201e7467e6cdab8b8ce9886741
Craig Hanson <craigahanson@gmail.com>
2020-03-21 09:05:29 +0000

    Copied pages etc over from client side blazor project (not quite working yet)

--- src/ProjectX.BlazorServer/Startup.cs
+++ src/ProjectX.BlazorServer/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ProjectX.BlazorServer.Data;
 
 namespace ProjectX.BlazorServer
 {
@@ -26,9 +25,9 @@ namespace ProjectX.BlazorServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
