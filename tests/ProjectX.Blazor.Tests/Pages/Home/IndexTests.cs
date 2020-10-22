7355c039242a13d67c08748fc097f01d2ffabb03
Craig Hanson <craigahanson@gmail.com>
2020-10-22 19:50:27 +0000

    Added Blazor test project with a simple test

---
+++ tests/ProjectX.Blazor.Tests/Pages/Home/IndexTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using ProjectX.Blazor.Pages.Home;
+using Bunit;
+
+namespace ProjectX.Blazor.Tests.Pages.Home
+{
+    public class IndexTests
+    {
+        [Test]
+        public void Index_DisplaysCorrectly()
+        {
+            //Arrange
+            using var context = new Bunit.TestContext();
+
+            //Act
+            var index = context.RenderComponent<Index>();
+
+            //Assert
+            var h1 = index.Find("h1");
+            Assert.That(h1, Is.Not.Null);
+            Assert.That(h1.TextContent, Is.EqualTo("Hello, world!"));
+        }
+    }
+}
