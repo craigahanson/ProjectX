974ab68cc8d72e825d1c66f9140dd4e225fd313c
Craig Hanson <craigahanson@gmail.com>
2021-01-30 16:37:09 +0000

    Removed unessessary properties from connection string

--- scripts/DatabaseFunctions.psm1
+++ scripts/DatabaseFunctions.psm1
@@ -101,7 +101,7 @@ function privateApplyMigrations {
     if($Configuration -eq "Development"){
         dotnet ef database update --project ..\src\ProjectX.Data.EntityFrameworkCore --startup-project ..\src\ProjectX.Data.EntityFrameworkCore -- "Server=$ServerInstance;Database=$DatabaseName;User ID=$ServerUsername;Password=$ServerPassword;Trusted_Connection=True;", 5
     } else {
-        dotnet ef database update --project src/ProjectX.Data.EntityFrameworkCore --startup-project src/ProjectX.Data.EntityFrameworkCore -- "Server=tcp:$ServerInstance,1433;Initial Catalog=$DatabaseName;Persist Security Info=False;User ID=$ServerUsername;Password=$ServerPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", 30
+        dotnet ef database update --project src/ProjectX.Data.EntityFrameworkCore --startup-project src/ProjectX.Data.EntityFrameworkCore -- "Server=tcp:$ServerInstance,1433;Initial Catalog=$DatabaseName;User ID=$ServerUsername;Password=$ServerPassword;", 30
     }
 }
 
